b91db8baab6c9c5ed8f2f44e3856b575d63f725a
NanePothier <nane.pothier@gmail.com>
2021-09-02 14:34:16 -0500

    Add a background image

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -4,10 +4,13 @@ import classes from './Header.module.css';
 
 const Header = (props) => {
   return (
-    <header className={classes.header}>
-      <h2>PizzaApp</h2>
-      <CartButton onClick={props.onShowCart}/>
-    </header>
+    <React.Fragment>
+      <header className={classes.header}>
+        <h2>PizzaApp</h2>
+        <CartButton onClick={props.onShowCart} />
+      </header>
+      <div className={classes['main-image']}></div>
+    </React.Fragment>
   );
 };
 
